265aca0b306406121680a48037410cfde31add2f
Maxim Medvedev <redrathnure@gmail.com>
2025-04-10 19:28:45 +0200

    feat: remove `copy` suffix

---
+++ cmd/fixNames.go
@@ -0,0 +1,69 @@
+/*
+Package cmd provides command handlers
+
+Copyright © 2020 Maksym Medvedev <redrathnure@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// fixNamesCmd represents the fixNames command
+var fixNamesCmd = &cobra.Command{
+	Use:   "fixNames [files]",
+	Short: "Normalize image names and remove -copy suffix",
+	Long: `Renaming files with the -copy suffix to shorten variations. 
+	files argument may be dir (process all files) or wildcards file names (process only matched files)`,
+	Args: cobra.RangeArgs(1, 1),
+	Run: func(cmd *cobra.Command, args []string) {
+		printCommandArgs(cmd, args)
+
+		files := extractPath(args, 0, ".")
+		log.Infof("files to process: '%s'", files)
+
+		log.Infof("recursively: %v", recursively)
+
+		log.Infof("dry ryn: %v", DryRun)
+
+		exifTool := getExifTool()
+
+		imgArgs := exifTool.newArgs()
+		tagName := "filename"
+
+		if DryRun {
+			tagName = "testname"
+		}
+		imgArgs.changeTag(tagName, "${filename;s/ - Copy/%-c/i}")
+
+		//Images and video
+		//imgArgs.forImages()
+		//imgArgs.forVideoMp4()
+		if recursively {
+			imgArgs.recursively()
+		}
+
+		imgArgs.src(files)
+
+		exifTool.exec(imgArgs)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(fixNamesCmd)
+
+	fixNamesCmd.Flags().BoolVarP(&recursively, "recursively", "r", false, "also analyze child directories")
+	fixNamesCmd.PersistentFlags().BoolVarP(&DryRun, "dry", "d", false, "Dry run")
+}
