038708d5cc42c72d0c50f229b4fc033938229816
Maxim Medvedev <redrathnure@gmail.com>
2026-04-06 20:55:18 +0200

    chore: MIT license

--- cmd/fix/dates.go
+++ cmd/fix/dates.go
@@ -1,20 +1,3 @@
-/*
-Package cmd provides command handlers
-
-Copyright © 2020 Maksym Medvedev <redrathnure@gmail.com>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package fix
 
 import (
@@ -29,7 +12,7 @@ var datesCmd = &cobra.Command{
 	Short: "Fix Exif/QuickTime dates",
 	Long: `Reads dates from file name and put into Exif and QuickTime metadata attributes. 
 	files argument may be dir (process all files) or wildcards file names (process only matched files)`,
-	Args: cobra.RangeArgs(1, 1),
+	Args:    cobra.RangeArgs(1, 1),
 	Aliases: []string{"fixDates"},
 	Run:     runFixDates,
 }
