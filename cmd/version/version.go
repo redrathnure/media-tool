78262e8b4380193a2d89826cba365c1bf64669f3
Maxim Medvedev <redrathnure@gmail.com>
2026-05-15 22:00:20 +0200

    chore: bump version to 1.13.0

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // It may be overridden during build. And it will be overridden during release
-var version = "1.12.0"
+var version = "1.13.0"
 var goVersion = "???"
 var goBuildPlatform = "???"
 
